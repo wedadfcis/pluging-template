c08f39324b37b8d34fef790c0e72e9716c2c9664
GitHub Action <action@github.com>
2021-04-06 11:36:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/wedadfcis/plugingtemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wedadfcis/plugingtemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wedadfcis.plugingtemplate.listeners
 
+import com.github.wedadfcis.plugingtemplate.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wedadfcis/plugingtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wedadfcis/plugingtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wedadfcis.plugingtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wedadfcis.plugingtemplate.MyBundle
 
 class MyApplicationService {
 
